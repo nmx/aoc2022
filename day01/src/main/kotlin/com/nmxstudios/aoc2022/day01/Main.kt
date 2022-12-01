e96f9fdf25e4cb1179fbbc03c3905ffef9bce895
Mike Nuss <mikenuss@gmail.com>
2022-11-30 22:54:29 -0800

    Day 1

---
+++ day01/src/main/kotlin/com/nmxstudios/aoc2022/day01/Main.kt
@@ -0,0 +1,53 @@
+package com.nmxstudios.aoc2022.day01
+
+import java.io.File
+import java.util.PriorityQueue
+
+fun main(args: Array<String>) {
+    val filename = "input.txt"
+    part1(filename)
+    part2(filename)
+}
+
+fun part1(filename: String) {
+    var max = 0
+    var sum = 0
+
+    File(filename).forEachLine {
+        if (it == "") {
+            if (sum > max) {
+                max = sum
+            }
+            sum = 0
+        } else {
+            sum += it.toInt()
+        }
+    }
+    if (sum > max) {
+        max = sum
+    }
+    println(max)
+}
+
+fun part2(filename: String) {
+    var sum = 0
+    val heap: PriorityQueue<Int> = PriorityQueue<Int>(Comparator.reverseOrder())
+
+
+    File(filename).forEachLine {
+        if (it == "") {
+            heap.add(sum)
+            sum = 0
+        } else {
+            sum += it.toInt()
+        }
+    }
+    heap.add(sum)
+
+    var result = 0
+    for (i in 1..3) {
+        result += heap.remove()
+    }
+
+    println(result)
+}
